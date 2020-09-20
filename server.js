9e3764c3c625d2c0835a77d37cbb2ac6c2f2f655
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-09-20 17:11:08 +0530

    Modified package.json, added socketio package & setup,added zoom icon

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
+const socketio = require('socket.io')
+const http = require('http')
 const express = require('express')
 const app =express()
+const server = http.createServer(app)
+const io = socketio(server)
 const port = process.env.PORT || 5000
-const { v4: uuidv4, v4} = require('uuid')
+const { v4 } = require('uuid')
 
 app.set('view engine','ejs')
 app.use(express.static('./public'))
+
+io.on('connection',(socket)=>{
+    socket.on('')
+})
 
 app.get('/',(req,res)=>{
     res.redirect(`/${v4()}`)
