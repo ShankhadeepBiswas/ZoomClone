72d062c1d57ab63af428397808cab0f8c1a51c5a
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-09-25 14:23:05 +0530

    Setting up peer calling and answering calls, styling the Zoom app

--- public/script.js
+++ public/script.js
@@ -1,32 +1,42 @@
-socket = io();
+const socket = io('/');
 const myVideo = document.createElement('video')
 const vGrid = document.getElementById('video-grid')
-const peer = new Peer(undefined,{
+var peer = new Peer(undefined,{
     path:'/peerjs',
     host:'/',
     port:'5000'
 })
-
-peer.on('open',id=>{
-    socket.emit('join-room',ROOM_ID,id);
-})
 navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true
-}).then((stream)=>{
+}).then(stream=>{
     addVideoStream(myVideo,stream)
+    socket.on('user-connected',(userId)=>{
+        connectToNewUser(userId,stream);
+    })
+    peer.on('call',call=>{
+        call.answer(stream);
+        const video = document.createElement('video')
+        call.on('stream',UserVideoStream=>{
+            addVideoStream(video,UserVideoStream);
+        })
+    })
+}) 
+peer.on('open',id=>{
+    socket.emit('join-room',ROOM_ID,id);
 })
-vGrid.append(myVideo) 
 const addVideoStream =(video,stream)=>{
     video.srcObject= stream;
     video.addEventListener('loadedmetadata',()=>{
       video.play()  
     })
+    vGrid.append(video)
 }
 
-socket.on('user-connected',(userId,stream)=>{
-    connectToNewUser(userId,stream);
-})
 const connectToNewUser=(userId,stream)=>{
-    console.log('New user!');
+    const call= peer.call(userId,stream)
+    const video= document.createElement('video')
+    call.on('stream', UserVideoStream=>{
+        addVideoStream(video,UserVideoStream)
+    })
 }
